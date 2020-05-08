2e5e76f0d0945677d8eccb3af51cf7d25fceb205
Jeff Wong <awole20@gmail.com>
2020-05-08 15:02:16 -0700

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Install font awesome pro fonts
+
+Your api key needs to be included:
+
+## Install plugin
+
+This plugin works a little differently than the other plugins
---
+++ plugin.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# name: discourse-fontawesome-pro
+# about: Install fontawesome pro icons for Discourse
+# version: 0.1
+# authors: Jeff Wong
+
+after_initialize do
+end
