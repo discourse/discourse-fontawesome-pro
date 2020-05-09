564580b35124a009ece26ad51893e76578113b4f
Jeff Wong <awole20@gmail.com>
2020-05-08 17:27:47 -0700

    Add install script

---
+++ scripts/install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if ! [ -x "$(command -v yarn)" ]; then
+    curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add - &&\
+        echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list &&\
+        apt update &&\
+        apt install -y yarn nodejs &&\
+        yarn install
+fi
