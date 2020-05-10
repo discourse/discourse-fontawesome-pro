103aa5213a8d41b69f32406242de8d3f1e554039
Jeff Wong <awole20@gmail.com>
2020-05-10 11:40:56 -0700

    Fix install scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,6 +3,6 @@ if ! [ -x "$(command -v yarn)" ]; then
     curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add - &&\
         echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list &&\
         apt update &&\
-        apt install -y yarn nodejs &&\
+        apt install -y yarn nodejs
 fi
 yarn install
