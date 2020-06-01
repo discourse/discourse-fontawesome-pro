252a2171509202f6cc5d445ed69d239d1661e93a
Jeff Wong <awole20@gmail.com>
2020-06-01 10:07:19 -0700

    Remove console statements

--- assets/javascripts/initializers/replacements.js
+++ assets/javascripts/initializers/replacements.js
@@ -4,14 +4,12 @@ export default {
   name: "replace-icons",
   initialize() {
     withPluginApi("0.8.14", api => {
-      console.log(Discourse.SiteSettings.fa_icon_style);
       let style = Discourse.SiteSettings.fa_icon_style;
       if (style == "regular") {
         return;
       }
       let prefixMap = { solid: "fas", light: "fal", duotone: "fad" };
       let prefix = prefixMap[style];
-      console.log(prefix);
       [
         "adjust",
         "address-book",
