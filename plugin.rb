6d7297c234d7ccf032a8da158c299f9e31b630ff
David Taylor <david@taylorhq.com>
2022-12-29 10:10:05 +0000

    DEV: Introduce syntax_tree for ruby formatting

--- plugin.rb
+++ plugin.rb
@@ -7,17 +7,20 @@
 # transpile_js: true
 # url: https://github.com/discourse/discourse-fontawesome-pro
 
-register_asset 'stylesheets/common/fontawesome-pro.scss'
+register_asset "stylesheets/common/fontawesome-pro.scss"
 
-require File.expand_path('lib/register_icons.rb', __dir__)
+require File.expand_path("lib/register_icons.rb", __dir__)
 
 after_initialize do
-  prefix_mapping = { 'brands' => 'fab',
-                     'light' => 'fal',
-                     'regular' => 'far',
-                     'duotone' => 'fad',
-                     'solid' => 'fas' }
-  sprite_source = "#{Rails.root}/plugins/discourse-fontawesome-pro/fontawesome-workspace/node_modules/@fortawesome/fontawesome-pro/sprites"
+  prefix_mapping = {
+    "brands" => "fab",
+    "light" => "fal",
+    "regular" => "far",
+    "duotone" => "fad",
+    "solid" => "fas",
+  }
+  sprite_source =
+    "#{Rails.root}/plugins/discourse-fontawesome-pro/fontawesome-workspace/node_modules/@fortawesome/fontawesome-pro/sprites"
   sprite_dest = "#{Rails.root}/vendor/assets/svg-icons/fontawesome-pro"
   FileUtils.mkdir_p(sprite_dest) unless Dir.exist?(sprite_dest)
   prefix_mapping.each do |style, prefix|
@@ -28,13 +31,11 @@ after_initialize do
     File.open(fsource) do |source_file|
       contents = source_file.read
       contents.gsub!(/<symbol id="(?<id>.*)"/, "<symbol id=\"#{prefix}-\\k<id>\"")
-      File.open(fname, 'w+') { |f| f.write(contents) }
+      File.open(fname, "w+") { |f| f.write(contents) }
     end
   end
 
   %w[fal far fad fas].each do |style|
-    RegisterIcons.icon_replacements.each do |icon|
-      register_svg_icon "#{style}-#{icon}"
-    end
+    RegisterIcons.icon_replacements.each { |icon| register_svg_icon "#{style}-#{icon}" }
   end
 end
