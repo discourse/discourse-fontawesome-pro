f90358715fc162faa3be5ed7dc5d5dd2008e3f2e
David Taylor <david@taylorhq.com>
2025-02-06 23:02:03 +0000

    DEV: Bump dependencies and fix linting (#71)
    * DEV: Bump dependencies and fix linting
    
    * Use pnpm for fontawesome-workspace
    
    * whitespace

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-if ! [ -x "$(command -v yarn)" ]; then
-    curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add - &&\
-        echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list &&\
-        apt update &&\
-        apt install -y yarn nodejs
-fi
-cd fontawesome-workspace && yarn install
+
+pnpm -C fontawesome-workspace install
